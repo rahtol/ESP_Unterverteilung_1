998498aab337ae7217fca9ae968163b910fa0216
Z0005HSM <lothar.becke@siemens.com>
2023-08-25 17:33:07 +0200

    Version 1.3 - changed resolution of küche to 1000 imp/kWh

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,8 @@ namespace ESP_Unterverteilung {
 
 //const char* version = "Project ESP_Unterverteilung, Version 1.0, 20.08.2023";
 //const char* version = "Project ESP_Unterverteilung, Version 1.1, 21.08.2023";
-const char* version = "Project ESP_Unterverteilung, Version 1.2, 21.08.2023";
+//const char* version = "Project ESP_Unterverteilung, Version 1.2, 21.08.2023";
+const char* version = "Project ESP_Unterverteilung, Version 1.3, 25.08.2023";
 
 }
 
@@ -38,7 +39,7 @@ void setup()
     // Initialize MqttS0Counters
     MessageOutput.println("Initialize MqttS0Counters... ");
     MqttS0Counters.init();
-    MqttS0Counters.addS0Counter("küche", 5, 2000);  // Küche, grün
+    MqttS0Counters.addS0Counter("küche", 5, 1000);  // Küche, grün
     MqttS0Counters.addS0Counter("herd", 4, 400);  // Herd, gelb
     MessageOutput.println("... done initialize MqttS0Counters... ");
 }
